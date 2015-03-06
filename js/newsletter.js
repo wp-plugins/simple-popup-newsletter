c6ce603501bc511689bb1abd51820dd6b0fde1f4
Varga Zsolt <Varga Zsolt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 10:05:55 +0000

    upload new
    git-svn-id: https://plugins.svn.wordpress.org/simple-popup-newsletter/trunk@1106694 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/newsletter.js
@@ -0,0 +1,80 @@
+
+var $j = jQuery.noConflict();
+ 
+$j(document).ready(function($) {
+
+	$('.popup-box-wrapper').on('click', function(event){
+		if( $(event.target).is('.popup-box-close') || $(event.target).is('.popup-box-wrapper') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+		}
+	});
+  
+	$('.popup-box-wrapper-unsub').on('click', function(event){
+		if( $(event.target).is('.popup-box-close') || $(event.target).is('.popup-box-wrapper-unsub') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+		}
+	});
+  
+	$('#popup_box_sub').on('click', function(event){
+			event.preventDefault();
+			$('.popup-box-wrapper').removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+      popup_box_form.submit();
+	});
+
+	$('#popup_box_exit').on('click', function(event){
+			event.preventDefault();
+			$('.popup-box-wrapper').removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+	});
+  
+	$('#popup_box_unsub').on('click', function(event){
+			event.preventDefault();
+			$('.popup-box-wrapper').removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+      popup_box_form_unsub.submit();
+	});
+
+	$('#popup_box_exit_unsub').on('click', function(event){
+			event.preventDefault();
+			$('.popup-box-wrapper-unsub').removeClass('is-visible');
+	    $('.popup-box').removeClass('anim');
+	});
+  
+	$('#subscribtion_button').on('click', function(event){
+	   event.preventDefault();
+	   $('.popup-box-wrapper').addClass('is-visible');
+	   $('.popup-box').addClass('anim');
+	});
+  
+	$('#unsubscribtion_button').on('click', function(event){
+	   event.preventDefault();
+	   $('.popup-box-wrapper-unsub').addClass('is-visible');
+	   $('.popup-box').addClass('anim');
+	});
+  
+	$('#all_checked').on('click', function(event){
+     $('.chk').attr('checked', this.checked);
+	});
+
+	$('#table_order').on('change', function(event){
+      subscribers_form.submit();
+	});
+  
+
+});
+
+function show_popup() {
+	 $j('.popup-box-wrapper').addClass('is-visible');
+	 $j('.popup-box').addClass('anim');
+}
+
+function show_popup_unsub() {
+	 $j('.popup-box-wrapper-unsub').addClass('is-visible');
+	 $j('.popup-box').addClass('anim');
+}
+
